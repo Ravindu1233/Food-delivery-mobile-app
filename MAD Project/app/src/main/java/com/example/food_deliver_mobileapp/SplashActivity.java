824de82fcd10162f0b1e40ff8f241793d07e7681
Oshadha Samarasinghe <124981711+OKSamarasinghe@users.noreply.github.com>
2024-08-14 15:52:45 +0530

    Splash Complete

---
+++ MAD Project/app/src/main/java/com/example/food_deliver_mobileapp/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.food_deliver_mobileapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay for 3 seconds and then start the MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
+    }
+}
